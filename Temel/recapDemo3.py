9011a5eae5ef3908b7add9b27406b704f2ff535d
Oğuz Gürkan Altunok <oguzgurkan2001@gmail.com>
2021-07-08 00:39:27 +0300

    08.07.2021

--- Temel/recapDemo3.py
+++ Temel/recapDemo3.py
@@ -1,14 +1,4 @@
-number = int(input("Sayı giriniz: "))
-asalMi = True
-
+number , asalMi = int(input("Sayı giriniz: ")), True
 for i in range(2,number):
-    if number%i == 0:
-        asalMi = False
-        break
-
-if asalMi == True:
-    print("Sayı Asal")
-else:
-    print("Sayı Asal Değil")
-
-    
+    if number%i == 0: asalMi = False, print(str(number)+" sayısı Asal Değil, " + str(i) +" sayısına bölünüyor.") ; break
+if asalMi == True: print(str(number)+" Sayı Asal")
